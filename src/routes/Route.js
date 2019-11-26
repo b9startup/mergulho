46469d06fd1b28aece8dc5bf55d019c455057607
Victor Batalha <madivcb@gmail.com>
2019-11-26 19:24:46 -0300

    Corrigido prop type e removido alias de argumento

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export default function RouteWrapper({
-  component: Component,
-  isProtected,
-  ...rest
-}) {
+export default function RouteWrapper({ component, isProtected, ...rest }) {
   const signed = false;
 
   if (!signed && isProtected) return <Redirect to="/login" />;
 
   if (signed && !isProtected) return <Redirect to="/" />;
 
-  return <Route {...rest} component={Component} />;
+  return <Route {...rest} component={component} />;
 }
 
 RouteWrapper.propTypes = {
-  component: PropTypes.element.isRequired,
+  component: PropTypes.func.isRequired,
   isProtected: PropTypes.bool,
 };
 
