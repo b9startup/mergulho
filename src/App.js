d04921e72dcaf615b89b8556afd9794056bb7a6c
Victor Batalha <madivcb@gmail.com>
2019-11-26 11:30:24 -0300

    Adicionado Siimple

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import 'siimple';
+
 function App() {
-  return <h1>Hello, React!</h1>;
+  return <div className="siimple-h1">Hello, Siimple!</div>;
 }
 
 export default App;
