e09b77a92aec775ce05584e4a3da0960191ee6b5
Victor Batalha <madivcb@gmail.com>
2019-11-26 22:14:17 -0300

    Estruturação da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,39 @@
 import React from 'react';
 
+import Navbar from '~/components/Navbar';
+import Banner from './Banner';
+import About from './About';
+import Newsletter from './Newsletter';
+import Archive from './Archive';
+import Post from './Post';
+
 export default function Home() {
-  return <div className="siimple-h1">Home</div>;
+  return (
+    <div className="siimple-content siimple-content-large siimple--px-2">
+      <Navbar />
+      <Banner />
+
+      <main>
+        <div className="siimple-grid-row">
+          <div id="left-side" className="siimple-grid-col siimple-grid-col--3">
+            <About />
+            <Newsletter />
+            <Archive />
+          </div>
+
+          <div id="right-side" className="siimple-grid-col siimple-grid-col--9">
+            <Post />
+            <Post />
+            <Post />
+          </div>
+        </div>
+      </main>
+
+      <div className="siimple-rule siimple--mt-5" />
+
+      <footer className="siimple--text-center siimple--py-4">
+        <span className="siimple-small">© 2019 B9 Startup</span>
+      </footer>
+    </div>
+  );
 }
