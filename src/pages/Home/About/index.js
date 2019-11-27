8531e32111f2f097123056f11ef865e25d579451
Victor Batalha <madivcb@gmail.com>
2019-11-26 22:13:56 -0300

    Criados componentes de informação

---
+++ src/pages/Home/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="siimple--mb-2 siimple--pb-4">
+      <div className="siimple-h4">Sobre mim</div>
+
+      <div className="siimple-paragraph siimple--text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </div>
+    </div>
+  );
+}
