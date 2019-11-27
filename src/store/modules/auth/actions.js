7cb2b1535df47d1519e493f0f3af2ccf1e815d72
Victor Batalha <madivcb@gmail.com>
2019-11-27 01:04:01 -0300

    Integração com a api na parte de autenticação

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -4,3 +4,16 @@ export function signInRequest(email, password) {
     payload: { email, password },
   };
 }
+
+export function signInSuccess(token) {
+  return {
+    type: '@auth/SIGN_IN_SUCCESS',
+    payload: { token },
+  };
+}
+
+export function signFailure() {
+  return {
+    type: '@auth/SIGN_FAILURE',
+  };
+}
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,9 +1,21 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
   const { email, password } = payload;
 
-  console.log(email, password);
+  try {
+    const response = yield call(api.post, '/sessions', { email, password });
+
+    const { token } = response.data;
+
+    yield put(signInSuccess(token));
+  } catch (error) {
+    yield put(signFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
